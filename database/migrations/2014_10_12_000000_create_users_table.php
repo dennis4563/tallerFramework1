22b40e51f8c24131203b955f80398040d063b492
Alumno15 <dennisrt056@gmail.com>
2018-08-22 16:43:23 -0300

    quinta

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('dni');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('pasword')
             $table->rememberToken();
             $table->timestamps();
         });
